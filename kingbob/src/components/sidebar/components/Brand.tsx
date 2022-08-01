def12f0554b63b750711b7cafea6b95b10057497
Wildan Gunawan <gwildan50@gmail.com>
2022-08-02 02:04:52 +0700

    feat: try to add PNG logo

--- kingbob/src/components/sidebar/components/Brand.tsx
+++ kingbob/src/components/sidebar/components/Brand.tsx
@@ -1,17 +1,18 @@
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
-  //   Chakra color mode
-  let logoColor = useColorModeValue("navy.700", "white");
-
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      <Image
+        src="https://static.wixstatic.com/media/04819c_67fe651ef39e425ba783ff9e8c996f60~mv2.png/v1/fill/w_278,h_88,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VATSEA_LOGO.png"
+        alt="VATSEA Logo"
+        objectFit={'fill'}
+        objectPosition={'center'}
+      />
       <HSeparator mb='20px' />
     </Flex>
   );
