03a3490e35a6abe1d23dc190596422593c4d4551
Wildan Gunawan <gwildan50@gmail.com>
2022-08-01 20:59:26 +0700

    fix: fix locale to US to fix hydration error

--- kingbob/src/components/calendar/MiniCalendar.tsx
+++ kingbob/src/components/calendar/MiniCalendar.tsx
@@ -27,6 +27,7 @@ export default function MiniCalendar(props: any) {
         value={value}
         selectRange={selectRange}
         view={"month"}
+        locale="en-US"
         tileContent={<Text color="brand.500"></Text>}
         prevLabel={<Icon as={MdChevronLeft} w="24px" h="24px" mt="4px" />}
         nextLabel={<Icon as={MdChevronRight} w="24px" h="24px" mt="4px" />}
