286e691e3385069e5b727286570fa1009d57edd4
Wildan Gunawan <gwildan50@gmail.com>
2022-08-05 15:38:03 +0700

    chore: move non-page files to components folder

--- kingbob/src/pages/admin/datatables/index.tsx
+++ kingbob/src/pages/admin/datatables/index.tsx
@@ -1,19 +1,19 @@
 // Chakra imports
 import { Box, SimpleGrid } from "@chakra-ui/react";
-import DevelopmentTable from "./components/DevelopmentTable";
-import CheckTable from "./components/CheckTable";
-import ColumnsTable from "./components/ColumnsTable";
-import ComplexTable from "./components/ComplexTable";
+import DevelopmentTable from "../../../components/datatables/components/DevelopmentTable";
+import CheckTable from "../../../components/datatables/components/CheckTable";
+import ColumnsTable from "../../../components/datatables/components/ColumnsTable";
+import ComplexTable from "../../../components/datatables/components/ComplexTable";
 import {
   columnsDataDevelopment,
   columnsDataCheck,
   columnsDataColumns,
   columnsDataComplex,
-} from "./variables/columnsData";
-import tableDataDevelopment from "./variables/tableDataDevelopment.json";
-import tableDataCheck from "./variables/tableDataCheck.json";
-import tableDataColumns from "./variables/tableDataColumns.json";
-import tableDataComplex from "./variables/tableDataComplex.json";
+} from "../../../components/datatables/variables/columnsData";
+import tableDataDevelopment from "../../../components/datatables/variables/tableDataDevelopment.json";
+import tableDataCheck from "../../../components/datatables/variables/tableDataCheck.json";
+import tableDataColumns from "../../../components/datatables/variables/tableDataColumns.json";
+import tableDataComplex from "../../../components/datatables/variables/tableDataComplex.json";
 import AdminLayout from "layouts/admin";
 
 export default function Settings() {
