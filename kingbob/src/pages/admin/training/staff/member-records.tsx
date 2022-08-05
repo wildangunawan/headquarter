410893fa45bafca769e3c75cb10991f9ad58f4eb
Wildan Gunawan <gwildan50@gmail.com>
2022-08-05 20:21:41 +0700

    feat: use all new components

--- kingbob/src/pages/admin/training/staff/member-records.tsx
+++ kingbob/src/pages/admin/training/staff/member-records.tsx
@@ -1,7 +1,10 @@
 import { Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import ExaminerList from "@components/training/staff/ExaminerList";
 import FutureTraining from "@components/training/staff/FutureTraining";
 import MemberList from "@components/training/staff/MemberList";
+import MentorList from "@components/training/staff/MentorList";
 import PastTraining from "@components/training/staff/PastTraining";
+import StudentList from "@components/training/staff/StudentList";
 import AdminLayout from "@layouts/admin";
 
 const MemberTrainingRecords = () => {
@@ -13,9 +16,12 @@ const MemberTrainingRecords = () => {
                 <Text color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
                     Member Training Records
                 </Text>
-                <MemberList />
                 <FutureTraining />
                 <PastTraining />
+                <StudentList />
+                <MemberList />
+                <MentorList />
+                <ExaminerList />
             </VStack>
         </AdminLayout>
     );
