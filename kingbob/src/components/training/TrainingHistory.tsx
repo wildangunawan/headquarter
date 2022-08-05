ae47266521357132d483c077ca2fd4cb099c18fe
Wildan Gunawan <gwildan50@gmail.com>
2022-08-05 20:39:11 +0700

    feat: enable training input by mentor

--- kingbob/src/components/training/TrainingHistory.tsx
+++ kingbob/src/components/training/TrainingHistory.tsx
@@ -16,6 +16,7 @@ import {
 import Card from "@components/card/Card";
 import NavLink from "next/link"
 import { useState } from 'react';
+import AddTrainingHistory from './modal/AddTrainingHistory';
 
 const TrainingHistory = ({ viewingMyself = false }: { viewingMyself?: boolean }) => {
     const textColor = useColorModeValue("secondaryGray.900", "white");
@@ -23,85 +24,98 @@ const TrainingHistory = ({ viewingMyself = false }: { viewingMyself?: boolean })
     // State
     const [showMePls, setShowMePls] = useState(false);
 
-    return (
-        <Card>
-            <Flex>
-                <Text
-                    me="auto"
-                    color={textColor}
-                    fontSize="xl"
-                    fontWeight="700"
-                    lineHeight="100%"
-                    px="15px" py="10px"
-                >
-                    Training History
-                </Text>
-                <Button colorScheme={"blue"} onClick={() => setShowMePls(true)}>Show example pls.</Button>
-            </Flex>
+    // Modal
+    const { onOpen, modal } = AddTrainingHistory();
 
-            {
-                showMePls
-                    ? <TableContainer>
-                        <Table variant='simple'>
-                            <Thead>
-                                <Tr>
-                                    <Th>#</Th>
-                                    <Th>Mentor</Th>
-                                    <Th>Date and time (UTC)</Th>
-                                    <Th>Actions</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td>123</Td>
-                                    <Td>
-                                        <NavLink href="/profile/1000000" passHref>
-                                            <Link color="brand.500">Kevin (1000000)</Link>
-                                        </NavLink>
-                                    </Td>
-                                    <Td>30 Aug 2022 12:00 - 30 Aug 2022 14:00</Td>
-                                    <Td>
-                                        <NavLink href="/admin/training/detail/abcd" passHref>
-                                            <Button as="a" colorScheme="blue">View</Button>
-                                        </NavLink>
-                                    </Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>123</Td>
-                                    <Td>
-                                        <NavLink href="/profile/1000000" passHref>
-                                            <Link color="brand.500">Kevin (1000000)</Link>
-                                        </NavLink>
-                                    </Td>
-                                    <Td>30 Aug 2022 12:00 - 30 Aug 2022 14:00</Td>
-                                    <Td>
-                                        <NavLink href="/admin/training/detail/abcd" passHref>
-                                            <Button as="a" colorScheme="blue">View</Button>
-                                        </NavLink>
-                                    </Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>123</Td>
-                                    <Td>
-                                        <NavLink href="/profile/1000000" passHref>
-                                            <Link color="brand.500">Kevin (1000000)</Link>
-                                        </NavLink>
-                                    </Td>
-                                    <Td>30 Aug 2022 12:00 - 30 Aug 2022 14:00</Td>
-                                    <Td>
-                                        <NavLink href="/admin/training/detail/abcd" passHref>
-                                            <Button as="a" colorScheme="blue">View</Button>
-                                        </NavLink>
-                                    </Td>
-                                </Tr>
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
-                    : <Center h={"100%"} w={"100%"} my="1em" px="15px" py="10px">
-                        {viewingMyself ? "You don't" : "This student doesn't"} have any training record.
-                    </Center>
-            }
-        </Card>
+    return (
+        <>
+            <Card>
+                <Flex>
+                    <Text
+                        me="auto"
+                        color={textColor}
+                        fontSize="xl"
+                        fontWeight="700"
+                        lineHeight="100%"
+                        px="15px" py="10px"
+                    >
+                        Training History
+                    </Text>
+                    <Button colorScheme={"blue"} onClick={onOpen}>Add training</Button>
+                    <Button colorScheme={"blue"} onClick={() => setShowMePls(true)}>Show example pls.</Button>
+                </Flex>
+
+                {
+                    showMePls
+                        ? <TableContainer>
+                            <Table variant='simple'>
+                                <Thead>
+                                    <Tr>
+                                        <Th>#</Th>
+                                        <Th>Mentor</Th>
+                                        <Th>Date and time (UTC)</Th>
+                                        <Th>vACC</Th>
+                                        <Th>Actions</Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td>123</Td>
+                                        <Td>
+                                            <NavLink href="/profile/1000000" passHref>
+                                                <Link color="brand.500">Kevin (1000000)</Link>
+                                            </NavLink>
+                                        </Td>
+                                        <Td>30 Aug 2022 12:00</Td>
+                                        <Td>Indonesia vACC (IDN)</Td>
+                                        <Td>
+                                            <NavLink href="/admin/training/detail/abcd" passHref>
+                                                <Button as="a" colorScheme="blue">View</Button>
+                                            </NavLink>
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>123</Td>
+                                        <Td>
+                                            <NavLink href="/profile/1000000" passHref>
+                                                <Link color="brand.500">Kevin (1000000)</Link>
+                                            </NavLink>
+                                        </Td>
+                                        <Td>30 Aug 2022 12:00</Td>
+                                        <Td>Indonesia vACC (IDN)</Td>
+                                        <Td>
+                                            <NavLink href="/admin/training/detail/abcd" passHref>
+                                                <Button as="a" colorScheme="blue">View</Button>
+                                            </NavLink>
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>123</Td>
+                                        <Td>
+                                            <NavLink href="/profile/1000000" passHref>
+                                                <Link color="brand.500">Kevin (1000000)</Link>
+                                            </NavLink>
+                                        </Td>
+                                        <Td>30 Aug 2022 12:00</Td>
+                                        <Td>Indonesia vACC (IDN)</Td>
+                                        <Td>
+                                            <NavLink href="/admin/training/detail/abcd" passHref>
+                                                <Button as="a" colorScheme="blue">View</Button>
+                                            </NavLink>
+                                        </Td>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+                        </TableContainer>
+                        : <Center h={"100%"} w={"100%"} my="1em" px="15px" py="10px">
+                            {viewingMyself ? "You don't" : "This student doesn't"} have any training record.
+                        </Center>
+                }
+            </Card>
+
+            {/* Modal */}
+            {modal}
+        </>
     );
 };
 
---
+++ kingbob/src/components/training/modal/AddTrainingHistory.tsx
@@ -0,0 +1,81 @@
+import { Button, Text, Flex, Input, Select, FormControl, FormLabel, Switch } from "@chakra-ui/react";
+import useModal from "@hooks/useModal";
+import Router from "next/router";
+import { useState } from "react";
+
+const AddTrainingHistory = () => {
+    // Request state
+    const [loading, setLoading] = useState(false);
+
+    const submitApplication = () => {
+        // Set loading
+        setLoading(true)
+
+        // TODO: Make request to API
+        // We'll use setTimeout for now
+        setTimeout(() => {
+            // Reset loading
+            setLoading(false)
+
+            // Close modal
+            onClose()
+
+            // Show toast that application was submitted
+            toast({
+                title: `Training data submitted`,
+                status: 'success',
+                position: 'bottom-right',
+                duration: 5000,
+                isClosable: true,
+            })
+
+            // Show toast that application was failed to submit
+            toast({
+                title: 'Failed to submit request',
+                description: "There is an error in our system. Please try again.",
+                status: 'error',
+                position: 'bottom-right',
+                duration: 5000,
+                isClosable: true,
+            })
+
+            Router.push("/admin/training/detail/abcd")
+        }, 2000)
+    }
+
+    const { onOpen, onClose, modal, toast } = useModal({
+        title: `Add training`,
+        body: <>
+            <Flex direction={"column"} gap={2} mb={4}>
+                <Text>Training at which vACC?</Text>
+                <Select>
+                    <option value='option1'>Hong Kong vACC</option>
+                    <option value='option2'>Indonesia vACC</option>
+                </Select>
+            </Flex>
+            <Flex direction={"column"} gap={2} mb={4}>
+                <Text>When did/will it happen?</Text>
+                <Input type={"datetime-local"} />
+            </Flex>
+            <FormControl display='flex' alignItems='center' gap={2}>
+                <Switch id='email-alerts' colorScheme='green' defaultChecked />
+                <FormLabel htmlFor='email-alerts' mb='0' fontSize={"sm"} fontWeight={400}>
+                    Open session detail after submission
+                </FormLabel>
+            </FormControl>
+        </>,
+        footer: <Button
+            colorScheme='blue'
+            size={"md"}
+            onClick={submitApplication}
+            isLoading={loading}
+            loadingText="Submitting"
+        >
+            Submit
+        </Button>
+    })
+
+    return { onOpen, modal };
+};
+
+export default AddTrainingHistory;
