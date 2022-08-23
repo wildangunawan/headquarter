c2a0d852ed16669bcae14cf69b27d336f0e7be0c
Wildan Gunawan <gwildan50@gmail.com>
2022-08-23 11:50:19 +0700

    feat: button to edit and delete faq

--- kingbob/src/pages/support/faq.tsx
+++ kingbob/src/pages/support/faq.tsx
@@ -6,62 +6,66 @@ import { ReactElement } from 'react';
 import CreateFaq from "../../components/support/modal/CreateFaq";
 
 const FaqPage = () => {
-    const textColor = useColorModeValue("secondaryGray.900", "white");
-    const faqs = [
-      {question: "question 1?", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-      {question: "question 2?", answer: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-      {question: "question 3?", answer: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
-      {question: "question 4?", answer: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-    ]
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const faqs = [
+    { question: "South East Asia Division - Question 1?", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { question: "South East Asia Division - Question 2?", answer: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
+    { question: "Indonesia vACC - Question 3?", answer: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+    { question: "HQ System - Question 4?", answer: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+  ]
 
-    // Modal
-    const {
-      onOpen: openCreateFaqModal,
-      modal: CreateFaqModal,
-      newQuestionAdded
-    } = CreateFaq();
+  // Modal
+  const {
+    onOpen: openCreateFaqModal,
+    modal: CreateFaqModal,
+    newQuestionAdded
+  } = CreateFaq();
 
-    return (
-      <>
-            <VStack spacing={"20px"} align="stretch">
-                {/* Header */}
-                <Flex justify={"space-between"}>
-                    <Text color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
-                        FAQ
-                    </Text>
-                    <Button as="a" colorScheme="blue" onClick={openCreateFaqModal}>Create FAQ</Button>
-                </Flex>
+  return (
+    <>
+      <VStack spacing={"20px"} align="stretch">
+        {/* Header */}
+        <Flex justify={"space-between"}>
+          <Text color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
+            Frequently Asked Questions (FAQs)
+          </Text>
+          <Button as="a" colorScheme="blue" onClick={openCreateFaqModal}>Create FAQ</Button>
+        </Flex>
 
-                {/* Data */}
-                <Card>
-                    <Accordion
-                        allowMultiple
-                        allowToggle
-                        defaultIndex={[0]} // to open first item by default
-                    >
-                        {faqs.map((item) => {
-                          return (<AccordionItem key={item.question}>
-                              <h2>
-                                  <AccordionButton>
-                                      <Box flex='1' textAlign='left'>
-                                        <b>{item.question}</b>
-                                      </Box>
-                                      <AccordionIcon />
-                                  </AccordionButton>
-                              </h2>
-                              <AccordionPanel pb={4}>
-                                  {item.answer}
-                              </AccordionPanel>
-                          </AccordionItem>
-                        );})}
-                    </Accordion>
-                </Card>
-            </VStack>
+        {/* Data */}
+        <Card>
+          <Accordion
+            allowMultiple
+            allowToggle
+          >
+            {faqs.map((item) => {
+              return (<AccordionItem key={item.question}>
+                <h2>
+                  <AccordionButton>
+                    <Box flex='1' textAlign='left'>
+                      <b>{item.question}</b>
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  {item.answer}
+                  <Flex mt={2} gap={1}>
+                    <Button colorScheme="orange">Edit</Button>
+                    <Button colorScheme="red">Delete</Button>
+                  </Flex>
+                </AccordionPanel>
+              </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </Card>
+      </VStack>
 
-            {/* Show modal */}
-            {CreateFaqModal}
-      </>
-    );
+      {/* Show modal */}
+      {CreateFaqModal}
+    </>
+  );
 };
 
 FaqPage.getLayout = (page: ReactElement) => <AdminLayout title="FAQ">{page}</AdminLayout>
