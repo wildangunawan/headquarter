35540f96439a2c726fba666a479a173a69032dcb
Wildan Gunawan <gwildan50@gmail.com>
2023-02-21 22:24:51 +0700

    chore: call seeders

--- stuart/database/seeders/DatabaseSeeder.php
+++ stuart/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(RegionSeeder::class);
+        $this->call(DivisionSeeder::class);
+        $this->call(SubdivisionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
