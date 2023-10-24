d3e60d959351ba18fe85b34b5cec37968110fe64
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2023-10-24 21:01:41 +0700

    feat: team rule and simple resource

--- stuart/app/Http/Resources/TeamResource.php
+++ stuart/app/Http/Resources/TeamResource.php
@@ -6,19 +6,33 @@ use App\Enums\TeamType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin \App\Models\Region | \App\Models\Division | \App\Models\Subdivision  */
 class TeamResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        if ($this->division_code !== null) $type = TeamType::SUBDIVISION;
-        else if ($this->region_code !== null) $type = TeamType::DIVISION;
+        if ($this->subdivision_code !== null) $type = TeamType::SUBDIVISION;
+        else if ($this->division_code !== null) $type = TeamType::DIVISION;
         else $type = TeamType::REGION;
 
-        return [
-            'name' => $this->name,
-            'code' => $this->code,
-            'type' => TeamType::getKey($type),
-        ];
+        switch ($type) {
+            case TeamType::REGION:
+                return [
+                    'name' => $this->region->name,
+                    'code' => $this->region->code,
+                    'type' => TeamType::getKey($type),
+                ];
+            case TeamType::DIVISION:
+                return [
+                    'name' => $this->division->name,
+                    'code' => $this->division->code,
+                    'type' => TeamType::getKey($type),
+                ];
+            case TeamType::SUBDIVISION:
+                return [
+                    'name' => $this->subdivision->name,
+                    'code' => $this->subdivision->code,
+                    'type' => TeamType::getKey($type),
+                ];
+        }
     }
 }
--- stuart/app/Rules/TeamExistRule.php
+++ stuart/app/Rules/TeamExistRule.php
@@ -11,37 +11,27 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class TeamExistRule implements ValidationRule
 {
-    /**
-     * All the data under validation.
-     *
-     * @var array<string, mixed>
-     */
-    protected array $data = [];
+    protected int $type;
 
     /**
-     * Set the data under validation.
-     *
-     * @param  array<string, mixed>  $data
+     * @param int $type
      */
-    public function setData(array $data): static
+    public function __construct(int $type)
     {
-        $this->data = $data;
-        return $this;
+        $this->type = $type;
     }
 
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $code = $this->data['assigned_to']['team']['code'];
-
-        switch ($this->data['assigned_to']['team']['type']) {
+        switch ($this->type) {
             case TeamType::REGION:
-                Region::where('code', $code)->exists() || $fail('The selected region is invalid.');
+                Region::where('code', $value)->exists() || $fail('The selected region is invalid.');
                 break;
             case TeamType::DIVISION:
-                Division::where('code', $code)->exists() || $fail('The selected division is invalid.');
+                Division::where('code', $value)->exists() || $fail('The selected division is invalid.');
                 break;
             case TeamType::SUBDIVISION:
-                Subdivision::where('code', $code)->exists() || $fail('The selected subdivision is invalid.');
+                Subdivision::where('code', $value)->exists() || $fail('The selected subdivision is invalid.');
                 break;
             default:
                 $fail('The selected team type is invalid.');
